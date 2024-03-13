4416fbd94c14dff2fb6bd39590fc6a403720e0c8
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-03-13 05:42:06 +0000

    frontend: remove tailwind shadcn ui use

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import UploadFile from "./components/upload";
+import UploadFile from "./components/uploadFile";
 
 function App() {
   return (
--- frontend/src/components/uploadFile/index.js
+++ frontend/src/components/uploadFile/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import ss from 'socket.io-stream';
 import { socket } from '../../socket';
 
