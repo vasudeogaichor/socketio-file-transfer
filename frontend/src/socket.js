065b44c96bc3a9d6f06071f317a045e3decaf0d7
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-12-11 00:21:49 +0530

    frontend: use env vars for backed api

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,7 +1,8 @@
 import { io } from 'socket.io-client';
 
-const URL = 'http://localhost:8504';
-
+const URL = process.env.REACT_APP_API_URL;
+// const URL = 'http://localhost:8504';
+// console.log('URL - ', URL)
 export const socket = io(URL, {
     autoConnect: true,
     extraHeaders: {
