627a96d0e5ff772a6ffa81dfa1e1ce51eaf594a0
Vasudeo Gaichor <vasudeogaichor@gmail.com>
2024-12-15 00:04:52 +0530

    frontend: add null check

--- frontend/src/components/auth/login/index.js
+++ frontend/src/components/auth/login/index.js
@@ -22,7 +22,7 @@ const Login = () => {
     });
 
     if (loginResult.success) {
-      setCookie('token', loginResult.data.token);
+      setCookie('token', loginResult.data?.token);
       navigate('/files');
     } else {
       setAlertInfo({
